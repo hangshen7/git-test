aff566c3385fb185f2ba3f9f5da7a4a060afb1af
hangshen7 <108789547+hangshen7@users.noreply.github.com>
2022-07-07 11:50:25 +0800

    hangshen

--- src/main/java/com/qi/git/GitTest.java
+++ src/main/java/com/qi/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("hot-fix test!");
         System.out.println("push yours!");
+        System.out.println("pull me!");
     }
 }
